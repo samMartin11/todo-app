09b3b197e2f42a335b8a7c9f2a3ffa5b1a52a908
samMartin11 <sougatasaha36@gmail.com>
2023-10-29 01:21:47 +0530

    3rd commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { configureStore } from "redux";
 import store from "./Redux/store";
 
 
