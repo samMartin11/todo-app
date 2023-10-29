1c1a50dfd927a226408d91c0dabc86a7c5dfb91f
samMartin11 <sougatasaha36@gmail.com>
2023-10-30 02:03:00 +0530

    data persistance feature

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -1,7 +1,31 @@
 function todos(state = [], action) {
+  const loadState = () => {
+    try {
+      const serializedState = localStorage.getItem("todoState");
+      if (serializedState == null) {
+        return undefined;
+      }
+      return JSON.parse(serializedState);
+    } catch (error) {
+      console.log("ERROR LOADING STATE -->", error);
+      return undefined;
+    }
+  };
+
+  const saveState = (state) => {
+    try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem("todoState", serializedState);
+    } catch (error) {
+      console.log("ERROR SAVING STATE -->", error);
+    }
+  };
+
+  let initialState = loadState();
+
   switch (action.type) {
     case "ADD_TODO":
-      return [
+      const newStateAdd = [
         ...state,
         {
           id: Date.now(),
@@ -9,19 +33,26 @@ function todos(state = [], action) {
           completed: false,
         },
       ];
+      saveState(newStateAdd);
+      return newStateAdd;
     case "DELETE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
+      const newStateDel =  state.filter((todo) => todo.id !== action.id);
+      saveState(newStateDel);
+      return newStateDel;
     case "EDIT_TODO":
-      return state.map((todo) =>
+      const newStateEdit = state.map((todo) =>
         todo.id === action.id ? { ...todo, text: action.text } : todo
       );
+      saveState(newStateEdit);
+      return newStateEdit;
     case "TASK_DONE":
-      return state.map((todo) =>
+      const newStateDone =  state.map((todo) =>
         todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
       );
-      console.log(state);
+      saveState(newStateDone);
+      return newStateDone;
     default:
-      return state;
+      return initialState ? initialState : state;
   }
 }
 
